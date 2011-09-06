6515aef07938bf2693ae52977f27793215037af4
phpfreak <reinier.van.loon@gmail.com>
2011-09-07 02:11:00 +0300

    Edited config/config.php via GitHub

--- config/config.php
+++ config/config.php
@@ -1,11 +1,3 @@
 <?php
-  define('DB_ADAPTER', 'mysql'); 
-  define('DB_HOST', 'localhost'); 
-  define('DB_USER', 'sharec_pp086'); 
-  define('DB_PASS', 'koffie10'); 
-  define('DB_NAME', 'sharec_pp'); 
-  define('DB_PREFIX', 'pp086_'); 
-  define('DB_CHARSET', 'utf8'); 
-  define('DB_PERSIST', 'false'); 
-  return true;
+
 ?>
